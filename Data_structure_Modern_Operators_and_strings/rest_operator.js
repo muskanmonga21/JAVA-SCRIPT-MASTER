817f4066c8611c1c7b9137d62f7f2235ac721631
muskan <muskanmonga521@gmail.com>
2024-07-26 20:10:53 +0530

    d19

---
+++ Data_structure_Modern_Operators_and_strings/rest_operator.js
@@ -0,0 +1,82 @@
+"use strict";
+
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Foccacia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // open 24 hours
+      close: 24,
+    },
+  },
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = "22:00",
+    address,
+  }) {
+    console.log(
+      `Order recieved! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
+  },
+
+  orderPizza: function (mainIngredients, ...otherIngredients) {
+    console.log(mainIngredients);
+    console.log(otherIngredients);
+  },
+};
+
+//SPREAD, beacuse it is on the right side of =
+const array = [1, 2, ...[6, 8]];
+console.log(array);
+
+//REST, beacuse it is on the left side of =
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+const [pizza, , risotto, ...otherfood] = [
+  ...restaurant.starterMenu,
+  ...restaurant.mainMenu,
+];
+console.log(pizza, risotto, otherfood);
+
+//OBJECTS
+const { sat, ...days } = restaurant.openingHours;
+console.log(days);
+
+//FUNCTION
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+};
+
+add(2, 4);
+add(3, 5, 1);
+add(1, 2, 3, 4, 5);
+
+const x = [3, 4, 2, 3];
+add(...x);
+
+restaurant.orderPizza("Cheese", "Basil", "Tomato");
+
+restaurant.orderPizza("Basil");
