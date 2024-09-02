533340f935a46724339c9730bf9e9617747d9822
muskan <muskanmonga521@gmail.com>
2024-09-02 19:14:51 +0530

    propagation

--- Advacnced DOM Bankist/script.js
+++ Advacnced DOM Bankist/script.js
@@ -159,3 +159,35 @@ setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
 // h1.onmouseenter = function (e) {
 //   alert('onmouseenter: Great! You are reading the heading :D');
 // };
+
+//rgb(255,255,255)
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
+
+console.log(randomColor(0, 255));
+
+document.querySelector('nav__link').addEventListener('click', function (e) {
+  this, (style.backgroundColor = randomColor());
+  e.peventDefault();
+  console.log('LINK', e.target, e.currentTarget);
+
+  // Stop Propagation
+  // e.stopPropagation();
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('CONTAINER', e.target, e.currentTarget);
+});
+
+document.querySelector('.nav').addEventListener(
+  'click',
+  function (e) {
+    this.style.backgroundColor = randomColor();
+    console.log('NAV', e.target, e.currentTarget);
+  },
+  true
+);
